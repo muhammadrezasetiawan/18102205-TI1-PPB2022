d086c503fb68bf92aa76bac48ce6441dae3e8c77
muhammadrezasetiawan <muhammadrezasetiawan33@gmail.com>
2022-04-24 16:11:40 +0700

    Praktikum6

---
+++ app/src/main/java/com/reza_18102205/practice6/MyData.kt
@@ -0,0 +1,11 @@
+package com.reza_18102205.practice6
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class MyData(
+    var name: String,
+    var description: String,
+    var photo: String
+) : Parcelable
